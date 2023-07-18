ec28d0c0ac536d53d44666db570fe3ee04e3c978
Denis Glazkov <glazzk.off@mail.ru>
2023-07-19 00:19:23 +0300

    static-json-builder: add more functionality into examples

--- examples/example.c
+++ examples/example.c
@@ -4,18 +4,26 @@
 
 int main(void)
 {
-    Json json = JsonObject(                /*  {                                 */
-        JsonProp("items", JsonArray(       /*     "items": [                     */
-            JsonNull(),                    /*         null,                      */
-            JsonBool(true),                /*         true,                      */
-            JsonInt(1),                    /*         1,                         */
-            JsonString("hello")            /*         "hello",                   */
-        ))                                 /*      ],                            */
-    );                                     /*  },                                */
+    Json json = JsonObject(                /*  {                  */
+        JsonProp("items", JsonArray(       /*     "items": [      */
+            JsonNull(),                    /*         null,       */
+            JsonBool(true),                /*         true,       */
+            JsonInt(1),                    /*         1,          */
+            JsonString("hello")            /*         "hello",    */
+        ))                                 /*      ],             */
+    );                                     /*  },                 */
 
-    char *string = json_stringify(json);   /*  {"items":[null,true,1,"hello"]}  */
-    printf("%s\n", string);
+    char *string = NULL;
+    char *buffer = malloc(json_stingified_size(json));
+
+    string = json_stringify(json);
+    json_stringify_into_buffer(json, buffer);
+
+    printf("%s\n", string);  /*  {"items":[null,true,1,"hello"]}  */
+    printf("%s\n", buffer);  /*  {"items":[null,true,1,"hello"]}  */
 
     free(string);
+    free(buffer);
+
     return 0;
 }
